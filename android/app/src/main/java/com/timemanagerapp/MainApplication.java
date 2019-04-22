97efb50484c594a23037b108f6e6fdfdbe7681d6
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-04-22 16:17:17 +0200

    codepush v2

--- android/app/src/main/java/com/timemanagerapp/MainApplication.java
+++ android/app/src/main/java/com/timemanagerapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.microsoft.appcenter.reactnative.push.AppCenterReactNativePushPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNGestureHandlerPackage(),
+            new AppCenterReactNativePushPackage(MainApplication.this),
             new VectorIconsPackage()
       );
     }
