4025c852fc154669a76e47ed37de87a2ff85471d
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-04-22 21:01:31 +0200

    monthCalendar development
    added "+" button (not working yet)
    month view + temporary design
    oslo reorganisation of containters

--- components/MonthCalendar.js
+++ components/MonthCalendar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 
 import Icon from "react-native-vector-icons/Ionicons";
@@ -51,45 +51,62 @@ class MonthCalendar extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Calendar
-          firstDay={1}
+        <View style={{ flex: 6, alignItems: "stretch" }}>
+          <Calendar
+            firstDay={1}
+            style={{
+              height: 350
+            }}
+            theme={{
+              backgroundColor: "#333333",
+              calendarBackground: "#333333",
+              textSectionTitleColor: "#b6c1cd",
+              selectedDayBackgroundColor: "#00adf5",
+              selectedDayTextColor: "#ffffff",
+              todayTextColor: "orange",
+              dayTextColor: "#d9e1e8",
+              textDisabledColor: "#555555",
+              dotColor: "#00adf5",
+              selectedDotColor: "#ffffff",
+              arrowColor: "orange",
+              monthTextColor: "orange",
+              textDayFontFamily: "monospace",
+              textMonthFontFamily: "monospace",
+              textDayHeaderFontFamily: "monospace",
+              textMonthFontWeight: "bold",
+              textDayFontSize: 16,
+              textMonthFontSize: 16,
+              textDayHeaderFontSize: 16
+            }}
+          />
+        </View>
+        <View style={{ flex: 1, paddingHorizontal: 10 }}>
+          <Button
+            title="wróc do logowania"
+            onPress={() => this.props.navigation.navigate("Login")}
+            color="#ff8833"
+          />
+        </View>
+        <View
           style={{
-            height: 350
-          }}
-          theme={{
-            backgroundColor: "#333333",
-            calendarBackground: "#333333",
-            textSectionTitleColor: "#b6c1cd",
-            selectedDayBackgroundColor: "#00adf5",
-            selectedDayTextColor: "#ffffff",
-            todayTextColor: "#00adf5",
-            dayTextColor: "#2d4150",
-            textDisabledColor: "#d9e1e8",
-            dotColor: "#00adf5",
-            selectedDotColor: "p",
-            arrowColor: "orange",
-            monthTextColor: "blue",
-            textDayFontFamily: "monospace",
-            textMonthFontFamily: "monospace",
-            textDayHeaderFontFamily: "monospace",
-            textMonthFontWeight: "bold",
-            textDayFontSize: 16,
-            textMonthFontSize: 16,
-            textDayHeaderFontSize: 16
-          }}
-        />
-        <TouchableOpacity
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            width: 100,
-            height: 100,
-            backgroundColor: "#fff",
-            borderRadius: 50
+            flex: 1,
+            padding: 5,
+            marginLeft: 285
           }}
         >
-          <Icon name={"md-add"} size={30} color="#01a699" />
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              width: 60,
+              height: 60,
+              backgroundColor: "orange",
+              borderRadius: 50
+            }}
+          >
+            <Icon name={"md-add"} size={30} color="white" />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -99,8 +116,9 @@ export default MonthCalendar;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     alignItems: "stretch",
-    justifyContent: "center"
+    marginTop: 50
   },
   text: {
     color: "white"
