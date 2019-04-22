04e4c2c672153eaac755622c2bdf4f433873e232
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-04-22 16:17:22 +0200

    Revert "codepush v2"
    This reverts commit 97efb50484c594a23037b108f6e6fdfdbe7681d6.

--- android/app/src/main/java/com/timemanagerapp/MainApplication.java
+++ android/app/src/main/java/com/timemanagerapp/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.microsoft.appcenter.reactnative.push.AppCenterReactNativePushPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNGestureHandlerPackage(),
-            new AppCenterReactNativePushPackage(MainApplication.this),
             new VectorIconsPackage()
       );
     }
