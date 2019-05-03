38478fd419a6bfeb13d019a92d39b44e806dc76a
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-05-03 15:35:14 +0200

    added Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,139 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    mColor: "white",
+    wColor: "white",
+    dColor: "white",
+    aColor: "white"
+  };
+
+  componentDidMount() {
+    switch (this.props.current) {
+      case "Month":
+        this.setState({ mColor: "orange" });
+        break;
+      case "Week":
+        this.setState({ wColor: "orange" });
+        break;
+      case "Day":
+        this.setState({ dColor: "orange" });
+        break;
+      case "Archive":
+        this.setState({ aColor: "orange" });
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.navbar}>
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => this.props.navigation.navigate("Month")}
+        >
+          <View style={styles.iconView}>
+            <Icon
+              name="calendar-blank"
+              size={30}
+              style={{ color: this.state.mColor }}
+            />
+            <Text
+              style={{
+                color: this.state.mColor,
+                fontSize: 10,
+                fontFamily: "Roboto-Bold"
+              }}
+            >
+              Miesiąc
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => this.props.navigation.navigate("Week")}
+        >
+          <View style={styles.iconView}>
+            <Icon
+              name="calendar-week"
+              size={30}
+              style={{ color: this.state.wColor }}
+            />
+            <Text
+              style={{
+                color: this.state.wColor,
+                fontSize: 10,
+                fontFamily: "Roboto-Bold"
+              }}
+            >
+              Tydzień
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => this.props.navigation.navigate("Day")}
+        >
+          <View style={styles.iconView}>
+            <Icon
+              name="calendar-today"
+              size={30}
+              style={{ color: this.state.dColor }}
+            />
+            <Text
+              style={{
+                color: this.state.dColor,
+                fontSize: 10,
+                fontFamily: "Roboto-Bold"
+              }}
+            >
+              Dzień
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => this.props.navigation.navigate("Archive")}
+        >
+          <View style={styles.iconView}>
+            <Icon
+              name="archive"
+              size={30}
+              style={{ color: this.state.aColor }}
+            />
+            <Text
+              style={{
+                color: this.state.aColor,
+                fontSize: 10,
+                fontFamily: "Roboto-Bold"
+              }}
+            >
+              Archiwum
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  navbar: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#3a3a3a"
+  },
+  iconView: {
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
+  }
+});
