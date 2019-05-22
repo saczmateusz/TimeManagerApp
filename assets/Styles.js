e4a84857915033887f65a4c391a011b6366febb1
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-05-22 23:10:30 +0200

    Create Styles.js
    but its not working yet XD

---
+++ assets/Styles.js
@@ -0,0 +1,36 @@
+import StyleSheet from "react-native";
+
+export const Styledds = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "stretch",
+    backgroundColor: "#f5f5f6",
+    paddingHorizontal: 10
+  },
+  scroll: {
+    flexDirection: "column"
+  },
+  dayTile: {
+    backgroundColor: "#f5f5f6",
+    borderRadius: 3,
+    margin: 5,
+    flex: 1
+  },
+  dayHeader: {
+    color: "#333",
+    fontSize: 20,
+    fontFamily: "Roboto-Bold"
+  },
+  taskTile: {
+    backgroundColor: "#e1e1e1",
+    flex: 1,
+    margin: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 3
+  },
+  taskText: {
+    color: "#333",
+    fontSize: 15
+  }
+});
