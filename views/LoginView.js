6c7fa0fa9ccfdf057e0a8dc29bef311f0da2623d
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-05-31 17:11:30 +0200

    directory cleanup

--- views/LoginView.js
+++ views/LoginView.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
 import LoginForm from "../containers/LoginForm";
 
 class LoginView extends Component {
